231a189f18d034652f1c27b813df61d4f7f87325
xed <xed@DESKTOP-K6NONND.localdomain>
2022-09-17 06:06:37 -0700

    Fixed minor bugs in path handling

--- tester.py
+++ tester.py
@@ -33,6 +33,11 @@ def test_cmd(exe_path, test, noout, i):
 	print(out)
 	return success
 
+def str_to_path(string):
+	if not "./" in string[:2] and not "/" in string[:1] and not ".." in string[:2] and not "~/" in string[:2]:
+			string = "./" + string
+	return string
+
 def col(str, col):
 	return f"\033[{col}m{str}\033[0m"
 
@@ -48,10 +53,14 @@ def catch_args():
 	interactive = False
 	file_path = "new_minishell_tester/tests"
 	exe_path = "./minishell"
+	help_msg = "Usage: python3 tester.py [-io] [-exe executable_path] [tests_file] [file_line]\n -i:\t\tInteractive mode\n -o:\t\tPrint output of tests\n -exe path:\tPath to your minishell executable\n tests_file:\tPath to the text file containing the tests, one per line (default: \"./new_minishell_tester/tests\")\n file_line:\tTests only the specified line of the file specified by file path\n\nArguments don't have a specific order"
+	if argc < 2:
+		print(help_msg)
+		exit(0)
 	while i != argc:
 		arg = str(sys.argv[i])
 		if arg == "-h":
-			print("Usage: python3 tester.py [-io] [-exe executable_path] [tests_file] [file_line]\n -i:\t\tInteractive mode\n -o:\t\tPrint output of tests\n -exe path:\tPath to your minishell executable\n tests_file:\tPath to the text file containing the tests, one per line (default: \"./new_minishell_tester/tests\")\n file_line:\tTests only the specified line of the file specified byt file path\n\nArguments don't have a specific order")
+			print(help_msg)
 			exit(0)
 		elif arg == "-o":
 			noout = False
@@ -61,16 +70,17 @@ def catch_args():
 			i += 1
 			if i >= argc:
 				error("Error: -exe flag requires one positional argument(path)")
-			exe_path = "./" + str(sys.argv[i]).lstrip("./")
+			exe_path = str_to_path(sys.argv[i])
 		elif arg.isdigit():
 			single = int(arg) - 1
 		else:
-			file_path = arg
+			file_path = str_to_path(arg)
 		i += 1
-	if not exists(file_path) or os.path.isdir(file_path):
+	if not exists(file_path) or os.path.isdir(file_path) or os.access(file_path, os.X_OK):
 		error("Error: '" + file_path + "' isn't a valid file")
 	if not exists(exe_path) or os.path.isdir(exe_path) or not os.access(exe_path, os.X_OK):
 		error("Error: '" + exe_path + "' isn't a valid file")
+	print(file_path, exe_path)
 	return noout, interactive, single, file_path, exe_path
 
 def main():
